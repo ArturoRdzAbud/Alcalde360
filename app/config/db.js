c75ff2c47cf39f1ebda598b9e259f13ccb56a9db
rolandoloera <roliclock@gmail.com>
2024-09-26 16:35:51 -0600

    intalling dotenv

--- app/config/db.js
+++ app/config/db.js
@@ -3,6 +3,16 @@ const dbConfig = {
   password: 'Nimdas2023@',
   database: 'Alcalde360_Des',
   server: 'enlaces.database.windows.net',
+
+  //cosas a instalar en el proyecto API node.js
+  //npm install nodemon
+  //    esto ayuda a que la api se reinicie automaticamente cada que hagan un cambio, como ocurre en el proyecto web+
+  //    sin necesidad de que esten reiniciando en forma "manual" la API
+  //npm install dotenv
+  //    permite cargar variables de entorno desde un archivo .env a tu aplicación para credenciales de Twilio
+  //    nota quiza valga la pena hacer lo mismo con las credenciales del sql que esta en db.js para mas seguridad
+
+
   pool: {
     max: 30,
     min: 0,
