9e7896b8c1e8df98927c5bdd89ad24444d6cb17b
ArturoRdzAbud <arturo.rodriguez.abud@gmail.com>
2025-01-28 13:32:14 -0600

    se suben cambios para envio de mensajes de whatsapp a participantes de la reunión y a responsables de las actividades cuando se guarda la ficha tecnica de la reunión.

--- app/models/GuardarFicha.js
+++ app/models/GuardarFicha.js
@@ -2,6 +2,96 @@ const mssql = require("mssql");
 const sqlConfig = require("../config/db");
 //Configuración de Twilio
 const twilio = require('../config/Twilio');
+const { format, parseISO } = require('date-fns'); 
+
+const { DOMParser } = require('xmldom');
+
+// Función para convertir XML a un array de objetos
+function parseXmlToParticipants(xmlData) { 
+  try {
+    
+   //console.log(xmlData)
+    const parser = new DOMParser();
+    const xmlDoc = parser.parseFromString(xmlData, 'text/xml');
+        
+    // Extraer nodos y convertirlos a una estructura de arreglos
+    const items = Array.from(xmlDoc.getElementsByTagName('Telefonos')).map((node) => {
+        return {
+          IdUsuario: node.getElementsByTagName('IdUsuario')[0]?.textContent,
+          Telefono: node.getElementsByTagName('Telefono')[0]?.textContent,
+          Nombre: node.getElementsByTagName('Nombre')[0]?.textContent,
+        };
+    });
+        
+    //console.log(items);
+    return items;
+    
+  } catch (error) {
+    console.error('Error al convertir XML a JSON:', error.message);
+    return null;
+  }
+}
+
+// Función para convertir XML a un array de objetos
+function parseXmlToAcuerdos(xmlData) { 
+  try {
+    
+   //console.log(xmlData)
+    const parser = new DOMParser();
+    const xmlDoc = parser.parseFromString(xmlData, 'text/xml');
+        
+    // Extraer nodos y convertirlos a una estructura de arreglos
+    const items = Array.from(xmlDoc.getElementsByTagName('Telefonos')).map((node) => {
+        return {
+          IdFichaTecnica: node.getElementsByTagName('IdFichaTecnica')[0]?.textContent,
+          IdAlcaldia: node.getElementsByTagName('IdAlcaldia')[0]?.textContent,
+          IdAcuerdo: node.getElementsByTagName('IdAcuerdo')[0]?.textContent,
+          Descripcion: node.getElementsByTagName('Descripcion')[0]?.textContent,
+          Num: node.getElementsByTagName('Num')[0]?.textContent,
+        };
+    });
+        
+    //console.log(items);
+    return items;
+    
+  } catch (error) {
+    console.error('Error al convertir XML a JSON:', error.message);
+    return null;
+  }
+}
+
+// Función para convertir XML a un array de objetos
+function parseXmlToActividades(xmlData) { 
+  try {
+    
+   //console.log(xmlData)
+    const parser = new DOMParser();
+    const xmlDoc = parser.parseFromString(xmlData, 'text/xml');
+        
+    // Extraer nodos y convertirlos a una estructura de arreglos
+    const items = Array.from(xmlDoc.getElementsByTagName('Telefonos')).map((node) => {
+        return {
+         Descripcion: node.getElementsByTagName('Descripcion')[0]?.textContent,
+         responsableIdCombo: node.getElementsByTagName('responsableIdCombo')[0]?.textContent,
+         FechaIni: node.getElementsByTagName('FechaIni')[0]?.textContent,
+         FechaFin: node.getElementsByTagName('FechaFin')[0]?.textContent,
+         ListoEditChk: node.getElementsByTagName('ListoEditChk')[0]?.textContent,
+
+        };
+    });
+        
+    //console.log(items);
+    return items;
+    
+  } catch (error) {
+    console.error('Error al convertir XML a JSON:', error.message);
+    return null;
+  }
+}
+
+
+
+
 
 exports.GuardarFicha = async (body) => {
   try {
@@ -35,6 +125,7 @@ exports.GuardarFicha = async (body) => {
         day: '2-digit'
     }).format(fecha);
 
+    
     const link = "https://localhost:5173/FichaTecnicaReunion?pnIdAlcaldia=" + body.pnIdAlcaldia + "&pnIdFichatecnica=" + fichaTecnicaReunion[0].IdFichaTecnica        
 
     // Enviar mensaje con información de la ficha técnica de la reunión al alcalde
@@ -44,10 +135,117 @@ exports.GuardarFicha = async (body) => {
     //console.log(nombreCiudadano)
     //console.log(whatsAppCiudadano)
     //console.log(whatsAppFuncionario)
+    
+    var TelefonosParticipantesXML = fichaTecnicaReunion[0].TelefonosParticipantesXML //"<person><id>1234</id><age>30</age><name>John Doe</name></person>";
+    var TelefonosResponsablesXML = fichaTecnicaReunion[0].TelefonosResponsablesXML //"<person><id>1234</id><age>30</age><name>John Doe</name></person>";
+    
+    //console.log(TelefonosResponsablesXML)
+    const Responsables = parseXmlToParticipants(TelefonosResponsablesXML);
+    const Participantes = parseXmlToParticipants(TelefonosParticipantesXML);
+    const Acuerdos = parseXmlToAcuerdos(body.psXmlAcuerdo);
+    const Actividades = parseXmlToActividades(body.psXmlActividades);
+    
+    //onsole.log(body.psXmlAcuerdo)
+    //console.log(body.psXmlActividades)
+    //console.log(Responsables)
+    //console.log(Participantes)
+    //console.log(Acuerdos)
+    //console.log(Actividades)
+  
+ 
+    if (Participantes && Acuerdos) {
+      EnviarMensajesAParticipantes(Participantes, Acuerdos, body.psTitulo, body.psFecha, body.psTema);       
+    }
+
+    if (Responsables && Actividades) {
+      EnviarMensajesAResponsables(Responsables, Actividades);
+    }
+      
+
     console.log('GUARDA CORRECTAMENTE')
     return fichaTecnicaReunion[0].IdFichaTecnica
   } catch (err) {
     console.log(err.message);
     return err.message;
   }
+};
+
+ 
+const EnviarMensajesAParticipantes = async (Participantes, Acuerdos, psTitulo, psFecha, psTema) => {
+  // Construir la lista de acuerdos una sola vez
+  const msgAcuerdos = Acuerdos.map(a => a.Descripcion).join(', ');
+
+  // Iterar sobre los participantes
+  for (const participante of Participantes) {
+    const msgParticipantes = `
+      Hola ${participante.Nombre}, te compartimos información de los siguientes acuerdos:
+      "${msgAcuerdos}", derivados de la Reunión "${psTitulo}" con el Tema: "${psTema}", llevado a cabo el: "${format(parseISO(psFecha), 'dd/MM/yyyy')}".
+    `.trim();
+
+    const whatsAppParticipante = '521'+ participante.Telefono
+
+    // Enviar mensaje a cada responsable de las actividades asignadas
+    await twilio.sendTextMessage(whatsAppParticipante, msgParticipantes);
+    //console.log(`Teléfono: ${participante.Telefono}, Mensaje WhatsApp: ${msgParticipantes}`);
+  }
+};
+
+
+
+const EnviarMensajesAResponsables = async (Responsables, Actividades) => {
+  let diasRestantes = 0;
+  let Estatus;
+
+  // Iterar sobre los Responsables
+  for (const responsable of Responsables) {
+    try {
+      // Buscar la actividad correspondiente al responsable
+      const miactividad = Actividades.find(dato => dato.responsableIdCombo === responsable.IdUsuario);
+
+      if (!miactividad) {
+        console.error(`No se encontró actividad para el responsable: ${responsable.Nombre}`);
+        continue; // Saltar al siguiente responsable si no hay actividad
+      }
+
+      // Determinar el estatus de la actividad
+      if (miactividad.ListoEditChk === 'true') {
+        Estatus = "Completada"; // 3
+      } else {
+        diasRestantes = calcularDiferenciaDias(miactividad.FechaIni, miactividad.FechaFin);
+
+        if (diasRestantes >= 0 && diasRestantes <= 1) {
+          Estatus = "Casi vencida"; // 2
+        } else if (diasRestantes > 1) {
+          Estatus = "Pendiente"; // 1
+        } else if (diasRestantes < 0) {
+          Estatus = "Vencida"; // 4
+        }
+      }
+
+      // Construir el mensaje
+      const msgResponsables = `
+        Hola ${responsable.Nombre}, te compartimos información de la actividad:
+        "${miactividad.Descripcion}", a la que fuiste asignado, con Estatus: "${Estatus}" y Fecha de vencimiento: "${format(parseISO(miactividad.FechaFin), 'dd/MM/yyyy')}".
+      `.trim();
+
+      const whatsAppResponsable = '521' + responsable.Telefono;
+
+      // Enviar mensaje a cada responsable de las actividades asignadas
+      await twilio.sendTextMessage(whatsAppResponsable, msgResponsables);
+      //console.log(`Teléfono: ${responsable.Telefono}, Mensaje WhatsApp: ${msgResponsables}`);
+    } catch (error) {
+      console.error(`Error enviando mensaje a ${responsable.Telefono}:`, error);
+    }
+  }
+};
+
+const calcularDiferenciaDias = (fechaIni, fechaFin) => {
+  const inicio = new Date(fechaIni);
+  const fin = new Date(fechaFin);
+
+  // Calcular la diferencia en milisegundos
+  const diferenciaMilisegundos = fin - inicio;
+
+  // Convertir de milisegundos a días y redondear hacia abajo
+  return Math.floor(diferenciaMilisegundos / (1000 * 60 * 60 * 24));
 };
